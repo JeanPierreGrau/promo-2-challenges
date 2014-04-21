e1cf610d3a746d807171b4dc5a36900dcef10056
jeanpierregrau <jeanpierregrau@gmail.com>
2014-04-21 10:13:22 +0200

    my_solution

--- 02-OOP/08-Cookbook/lib/controller.rb
+++ 02-OOP/08-Cookbook/lib/controller.rb
@@ -1,9 +1,29 @@
 require_relative 'cookbook'
 
 class Controller
+
   def initialize(file)
     # Here you should instantiate the Cookbook model with the file
+    @cookbook = Cookbook.new(file)
+  end
+    # TODO: Implement the methods to retrieve, add, and delete recipes through the model
+
+  def list_recipes
+    @cookbook.recipes
   end
 
-  # TODO: Implement the methods to retrieve, add, and delete recipes through the model
+
+  def add(recipe)
+    cookbook.create(recipe)
+  end
+
+
+  def delete_recipe(index)
+    recipe_to_delete = @cookbook.find(index)
+    @cookbook.destroy(index)
+    recipe_to_delete
+  end
+
 end
+
+
--- 02-OOP/08-Cookbook/lib/ui.rb
+++ 02-OOP/08-Cookbook/lib/ui.rb
@@ -1,4 +1,5 @@
 class UI
+
   TASKS = {
     list: "- List all recipes [list]",
     add:  "- Add a new recipe [add]",
@@ -14,27 +15,37 @@ class UI
   def list
     # TODO: call the appropriate controller method with the proper argument(s)
     # TODO: format and display the retrieved data in a numbered list
+    @controller.list_recipes.each_with_index do |recipe, index|
+      puts "#{index + 1}. #{recipe} (miam)"
+    end
   end
 
   def add
-    # TODO: ask the user a recipe name
-    # TODO: call the appropriate controller method with the proper argument(s)
+    p "What recipe do you want to delete?"
+    controller.add(user_input)
   end
+
 
   def del
     # TODO: ask the user a recipe index
     # TODO: call the appropriate controller method with the proper argument(s)
+    puts "Which recipe do you want to delete? (enter <id>)"
+    index = gets.chomp.to_i
+    deleted_recipes = @controller.delete_recipe(index - 1)
+    puts "The recipe #{deleted_recipes} has been deleted"
   end
 
   def exit
     # TODO: exit the program
     # Hint: Take a look at the display method !
+    @running = false
   end
 
   def user_input
     # TODO: Get the user input and return it
     # [OPTIONAL] You can think of the case where the user
     # enters a wrong choice.
+    user_answer = gets.chomp
   end
 
   def display
@@ -67,7 +78,16 @@ class UI
   #   dispatch("add") => Will call the `add` method in the current class
   #
   # To understand this, read the doc : http://ruby-doc.org/core-2.1.1/Object.html#method-i-send
+
   def dispatch(task)
     self.send(task.to_sym)
   end
 end
+
+
+
+
+
+
+
+
